bdc0f34763e843793f5fa3c08c594d7fefb9cce8
protimab <protimabanerjee@gmail.com>
2023-07-30 20:15:26 -0400

    Buttons

--- src/Timer.js
+++ src/Timer.js
@@ -1,5 +1,8 @@
 import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+import PlayButton from './PlayButton';
+import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
 
 const red = '#f54e4e'; 
 const green = '#4aec8c'
@@ -16,6 +19,13 @@ function Timer() {
         pathColor: red,
         tailColor: 'rgba(255,255,255,.2)',
         })}  />
+        <div style= {{marginTop: '20px'}}>
+            <PlayButton />
+            <PauseButton />
+        </div>
+        <div style= {{marginTop: '20px'}}>
+            <SettingsButton />
+        </div>
       </div>
     </div>
     );
