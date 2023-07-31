f030761c8caecb8c16bceafa07e11206488df4be
protimab <protimabanerjee@gmail.com>
2023-07-31 17:47:01 -0400

    slider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Timer from "./Timer";
+import Settings from "./Settings";
+import {useState} from "react";
 
 function App() {
+  const [showSettings, setShowSettings] = useState(true);
   return (
     <main>
+      {showSettings ? <Settings /> : <Timer />}
+
       <Timer />
-    </main>
+    </main> 
   );
 }
 
