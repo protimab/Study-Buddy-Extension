09577d80e31133c4f130751b7b3aaccdd4d0d20e
protimab <protimabanerjee@gmail.com>
2023-08-16 12:37:48 -0400

    plant

--- src/Timer.js
+++ src/Timer.js
@@ -5,6 +5,10 @@ import PauseButton from "./PauseButton";
 import SettingsButton from "./SettingsButton";
 import {useContext, useState, useEffect, useRef} from "react";
 import SettingsContext from "./SettingsContext";
+import plantStage1 from "./small_plant.png";
+import plantStage2 from "./sapling.jpeg";
+import plantStage3 from "./tree.png";
+import plantStage4 from "./sapling.jpeg";
 
 const red = '#f54e4e';
 const green = '#4aec8c';
@@ -14,7 +18,9 @@ function Timer() {
   const [isPaused, setIsPaused] = useState(true);
   const [mode, setMode] = useState('work'); 
   const [secondsLeft, setSecondsLeft] = useState(0);
-
+  const [plantStage, setPlantStage] = useState(0);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  
   const secondsLeftRef = useRef(secondsLeft);
   const isPausedRef = useRef(isPaused);
   const modeRef = useRef(mode);
@@ -25,33 +31,44 @@ function Timer() {
   }
 
   useEffect(() => {
-
     function switchMode() {
       const nextMode = modeRef.current === 'work' ? 'break' : 'work';
       const nextSeconds = (nextMode === 'work' ? settingsInfo.workMinutes : settingsInfo.breakMinutes) * 60;
-
+  
       setMode(nextMode);
       modeRef.current = nextMode;
-
+  
       setSecondsLeft(nextSeconds);
       secondsLeftRef.current = nextSeconds;
+  
+      setPlantStage(prevStage => (prevStage + 1) % 4);
     }
-
+  
     secondsLeftRef.current = settingsInfo.workMinutes * 60;
     setSecondsLeft(secondsLeftRef.current);
-
+  
     const interval = setInterval(() => {
       if (isPausedRef.current) {
         return;
       }
       if (secondsLeftRef.current === 0) {
-        return switchMode();
+        switchMode();
       }
       tick();
-    },1000);
+
+      setElapsedTime(prevTime => prevTime + 1);
+    }, 100);
 
     return () => clearInterval(interval);
   }, [settingsInfo]);
+
+  useEffect(() => {
+    if (modeRef.current === 'work' && elapsedTime >= 120) {
+      setElapsedTime(0);
+      setPlantStage(prevStage => (prevStage + 1) % 4);
+    }
+  }, [elapsedTime]);
+  
 
   const totalSeconds = mode === 'work'
     ? settingsInfo.workMinutes * 60
@@ -64,32 +81,42 @@ function Timer() {
     seconds = '0'+seconds;
 
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-    <div style={{ width: 200, height: 200 }}>
-      <CircularProgressbar
-        value={percentage}
-        text={minutes + ':' + seconds}
-        styles={buildStyles({
-        textColor:'#fff',
-        pathColor:mode === 'work' ? red : green,
-        tailColor:'rgba(255,255,255,.2)',
-        textSize: '10px',
-        textStyle:  {
-            fontFamily: 'Courier New', 
-            fontSize: '10px', 
-        },
-      })} />
-      <div style={{marginTop:'20px'}}>
-        {isPaused
-          ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} />
-          : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} />}
-      </div>
-      <div style={{marginTop:'30px'}}>
-        <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
-      </div>
-    </div>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'flex-start', alignItems: 'center', height: '100vh' }}>
+      <div style={{ marginTop: '20px', width: 300, height: 300, alignItems: 'flex-start' }}>
+        <img src={
+              plantStage === 0 ? plantStage1 :
+              plantStage === 1 ? plantStage2 :
+              plantStage === 2 ? plantStage3 :
+              plantStage4
+            } alt="Plant Stage" style={{ width: '100%', height: 'auto' }} />
+        </div>
+        
+        <div style={{ marginTop: '20px', width: 250, height: 250 }}>
+          <CircularProgressbar
+            value={percentage}
+            text={minutes + ':' + seconds}
+            styles={buildStyles({
+            textColor:'#fff',
+            pathColor:mode === 'work' ? red : green,
+            tailColor:'rgba(255,255,255,.2)',
+            textSize: '10px',
+            textStyle:  {
+                fontFamily: 'Courier New', 
+                fontSize: '10px', 
+            },
+          })} />
+        </div>
+
+        <div style={{marginTop:'30px'}}>
+          {isPaused
+               ? <PlayButton onClick={() => { setIsPaused(false); isPausedRef.current = false; }} style={{ width: '40px', height: '40px' }} />
+               : <PauseButton onClick={() => { setIsPaused(true); isPausedRef.current = true; }} style={{ width: '40px', height: '40px' }} />}
+        </div>
+
+        <div style={{marginTop:'10px'}}>
+          <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
+        </div>
     </div>
   );
 }
-
 export default Timer;
